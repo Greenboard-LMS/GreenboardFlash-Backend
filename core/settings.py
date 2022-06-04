e010c0896700e7d452901cb8ae3d048d6975d783
Silas Wolf <wolf.silas@icloud.com>
2022-06-04 19:28:38 +0200

    initializing flashcards app

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Internal
+    'flashcards',
+
     # Third party
     'corsheaders',
     'rest_framework',
